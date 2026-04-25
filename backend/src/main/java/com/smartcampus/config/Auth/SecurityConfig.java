a13d2121be6486505fa948647a0473f8eb1a116b
theekshana56 <Theekshanagamage056@gmail.com>
2026-04-26 01:19:46 +0530

    give cores access to local host port number 3000

--- backend/src/main/java/com/smartcampus/config/Auth/SecurityConfig.java
+++ backend/src/main/java/com/smartcampus/config/Auth/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174" , "http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
